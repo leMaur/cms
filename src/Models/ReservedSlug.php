d5b9969b1b428e175051ec1440adf1afd4592793
Maurizio <maurizio.lepora@gmail.com>
2021-03-31 22:09:29 +0200

    wip

--- src/Models/ReservedSlug.php
+++ src/Models/ReservedSlug.php
@@ -11,6 +11,11 @@ class ReservedSlug
     public const HOMEPAGE = '@home';
     public const SITEMAP = '@sitemap';
 
+    private static array $lookup = [
+        '@home' => '/',
+        '@sitemap' => '/sitemap.xml',
+    ];
+
     public static function list(): Collection
     {
         $reflector = new ReflectionClass(static::class);
@@ -18,5 +23,16 @@ class ReservedSlug
         return collect($reflector->getConstants())
             ->mapWithKeys(fn ($value, $key) => [$value => Str::lower($key)])
             ->toBase();
+    }
+
+    public static function handle(string $slug): string
+    {
+        $reflector = new ReflectionClass(static::class);
+
+        if (collect($reflector->getConstants())->values()->containsStrict($slug)) {
+           return static::$lookup[$slug];
+        }
+
+        return $slug;
     }
 }
