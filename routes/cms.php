815604556880d770ab221dbfb7005daa782e561a
Maurizio <maurizio.lepora@gmail.com>
2021-06-07 21:09:22 +0200

    wip

--- routes/cms.php
+++ routes/cms.php
@@ -4,10 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Lemaur\Cms\Http\Controllers\PageController;
 
 Route::macro('cms', function (string $name, string $prefix): void {
-    $domain = parse_url(config('app.url', ''), PHP_URL_HOST);
-
-    Route::domain($domain)
-        ->prefix($prefix)
+    Route::prefix($prefix)
         ->group(function () use ($name) {
             $reservedUri = (array) config('cms.reserved_uri', []);
             $regex = $reservedUri !== [] ? '^(?!'.implode('|', $reservedUri).')(.*)$' : '.*';
